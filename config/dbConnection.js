ff2b2470b31f31b22ddf9bbda22be4f8205cffde
Kunalsahuji <ksahu0103@gmail.com>
2025-01-07 17:33:54 +0530

    remove commented-out database connection code and log statement

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -3,17 +3,10 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.CONNECTION_STRING)
-        console.log("Database Connect")
+        // console.log("Database Connect")
     } catch (error) {
         console.log(error)
         process.exit(1)
     }
 }
 module.exports = connectDB
-
-// const mongoose = require('mongoose');
-// mongoose.connect("mongodb://0.0.0.0/REAL-ESTATE").then(() => {
-//     console.log("DB Connected!")
-// }).catch((err) => {
-//     console.log(err)
-// });
